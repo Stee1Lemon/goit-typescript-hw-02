cd5e4a99288c5b9140d74b94e4cd7c8be2b1c19c
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-01-21 17:09:25 +0000

    generics

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -2,9 +2,12 @@
   У вас є функція merge, яка поєднує два об'єкти. 
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
+type Obj = {
+  [keyName: string]: any;
+};
 
-function merge (objA, objB) {
+function merge<T extends Obj, K extends Obj>(objA: T, objB: K): T & K {
   return Object.assign(objA, objB);
 }
 
-export {}
+export {};
